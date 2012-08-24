adc4a54ed82a50b9daabf64a2198dd13e14602ce
Heartbroken <Heartbroken>
2012-08-25 01:08:05 +0300

    float2 unit tests

--- heart/math/float2.hpp
+++ heart/math/float2.hpp
@@ -14,7 +14,6 @@ namespace heart
 
 		inline float2& operator = (const float2 &_v) { x = _v.x; y = _v.y; return *this; }
 
-
 		friend inline float2 neg(const float2 &_v) { return float2(-_v.x, -_v.y); }
 
 		friend inline float2 add(const float2 &_a, const float2 &_b) { return float2(_a.x + _b.x, _a.y + _b.y); }
@@ -24,18 +23,18 @@ namespace heart
 
 		friend inline float2 mul(const float2 &_a, float _s) { return float2(_a.x * _s, _a.y * _s); }
 		friend inline float2 mul(float _s, const float2 &_b) { return float2(_s * _b.x, _s * _b.y); }
-		friend inline float2 operator * (float _s, const float2 &_b) { return mul(_s, _b); }
 		friend inline float2 div(const float2 &_a, float _s) { return float2(_a.x / _s, _a.y / _s); }
 		friend inline float2 div(float _s, const float2 &_b) { return float2(_s / _b.x, _s / _b.y); }
-		friend inline float2 operator / (float _s, const float2 &_b) { return div(_s, _b); }
 
-		friend inline float dot(const float2 &_a, const float2 &_b) { return _a.x * _b.x + _a.y * _b.x; }
+		friend inline float dot(const float2 &_a, const float2 &_b) { return _a.x * _b.x + _a.y * _b.y; }
 
 		friend inline float cross(const float2 &_a, const float2 &_b) { return _a.x * _b.y - _a.y * _b.x; }
 		friend inline float2 cross(const float2 &_v) { return float2(-_v.y, _v.x); }
 
 		friend inline float length2(const float2 &_v) { return dot(_v, _v); }
 		friend inline float length(const float2 &_v) { return sqrtf(length2(_v)); }
+
+		friend inline float2 normal(const float2 &_v) { return _v / length(_v); }
 
 
 		inline float2 operator - () { return neg(*this); }
@@ -48,10 +47,13 @@ namespace heart
 		inline float2& operator *= (const float2 &_v) { return *this = mul(*this, _v); }
 		inline float2 operator / (const float2 &_v) const { return div(*this, _v); }
 		inline float2& operator /= (const float2 &_v) { return *this = div(*this, _v); }
+
 		inline float2 operator * (float _s) const { return mul(*this, _s); }
 		inline float2& operator *= (float _s) { return *this = mul(*this, _s); }
+		friend inline float2 operator * (float _s, const float2 &_b) { return mul(_s, _b); }
 		inline float2 operator / (float _s) const { return div(*this, _s); }
 		inline float2& operator /= (float _s) { return *this = div(*this, _s); }
+		friend inline float2 operator / (float _s, const float2 &_b) { return div(_s, _b); }
 	};
 
 }
